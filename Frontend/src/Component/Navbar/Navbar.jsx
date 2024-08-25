6363bdaaddb42980ea7e5f1f35625a228828033b
Shraddha <applicantshraddha286@gmail.com>
2024-08-25 22:58:51 +0530

    add

--- Frontend/src/Component/Navbar/Navbar.jsx
+++ Frontend/src/Component/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import { BsSearch } from "react-icons/bs";
 
 
 const Navbar = () => {
-    const user= false
+    const user = false
     return (
         <div className='flex justify-between px-3 py-4 border-b-4 items-center'>
             <h1 className='text-lg md:text-xl font-extrabold text-blue-800 mi-2'><Link to="/">Connect Blog</Link></h1>
@@ -14,6 +14,7 @@ const Navbar = () => {
             </div>
 
             <div className='flex items-center justify-center space-x-2 md:space-x-4 px-3 sm:px-0'>
+                <h3><Link to="/posts">New Post</Link></h3>
                 {user ? <h3><Link to="/posts">New Post</Link></h3> : <h3><Link to="/login">Login</Link></h3>}
                 {user ? <h3>Profile</h3> : <h3><Link to="/register">Register</Link></h3>}
             </div>
